07d276fc19488c7ab9aa2cc5ed5818f1dd55adfa
raul <raul.arabaolaza@getsetpay.com>
2016-07-30 22:29:28 +0200

    Initial commit, empty skeleton generated from start.spring.io

---
+++ src/main/java/com/arabaolaza/pressureserver/ServerBackendApplication.java
@@ -0,0 +1,12 @@
+package com.arabaolaza.pressureserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBackendApplication.class, args);
+	}
+}
